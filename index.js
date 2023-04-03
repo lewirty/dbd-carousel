eef6f0cd6c620a06f8b3bf42bc62268bbf354d7d
lewirty <lucianolukaszuk@gmail.com>
2023-04-03 10:11:46 -0300

    erro js resolvido

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const botoescarrossel = document.querySelectorAll('.botao');
 const imagens = document.querySelectorAll('.imagem');
 botoescarrossel.forEach((botao, indice) => {
@@ -18,24 +17,3 @@ function desmarcarBotao() {
     const botaoselecionado = document.querySelector('.selecionado');
     botaoselecionado.classList.remove('selecionado');
 }
-=======
-const botoescarrossel = document.querySelectorAll('.botao');
-const imagens = document.querySelectorAll('.imagem');
-botoescarrossel.forEach((botao, indice) => {
-    botao.addEventListener('click', () => {
-        desmarcarBotao();
-
-        botao.classList.add('selecionado')
-
-        const imagemativa = document.querySelector('.ativa')
-        imagemativa.classList.remove('ativa')
-
-        imagens[indice].classList.add('ativa')
-    })
-})
-
-function desmarcarBotao() {
-    const botaoselecionado = document.querySelector('.selecionado');
-    botaoselecionado.classList.remove('selecionado');
-}
->>>>>>> 56be19794b0167367ff396d76c2154bea27a0fc7
